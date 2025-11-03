00a2d47f872431c464ca82db25008326477ed815
KHMong <kelheng04@gmail.com>
2025-11-04 02:57:58 +0800

    Update buttons

---
+++ frontend/src/components/common/BackButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoIosArrowBack } from "react-icons/io";
+import './BackButton.css';
+
+const BackButton = ({
+  to,
+  icon,
+  type = 'button',
+  ...rest
+}) => {
+  // Combine all the classes together
+  const classes = 'back-btn text-decoration-none d-inline-block mb-4';
+
+  // Link button
+  return (
+      <Link to={to} className={classes} {...rest}>
+        <IoIosArrowBack/> Back to Venues
+      </Link>
+    );
+};
+
+export default BackButton;
