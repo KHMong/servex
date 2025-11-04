030aa831f660905bb2973a49517a50b5a95ddab3
KHMong <kelheng04@gmail.com>
2025-11-04 19:36:17 +0800

    Add N/A image, Remove CourtBookingPage.css

--- frontend/src/utils/imageUrl.js
+++ frontend/src/utils/imageUrl.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-const PLACEHOLDER_IMAGE = 'https://via.placeholder.com/300x200';
+const PLACEHOLDER_IMAGE = 'https://placehold.co/300x200/FFF/64748B?text=N/A';
 
 export const getImageUrl = (relativePath) => {
   if (!relativePath) {
