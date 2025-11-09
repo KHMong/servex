4ba7d9aa94fb4b6832444eab406f809a1be97b63
KHMong <kelheng04@gmail.com>
2025-11-09 18:16:11 +0800

    Gray BG for /info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavbarComponent from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
 function App() {
+  const location = useLocation();
+
+  const needGrayBg = location.pathname.startsWith('/info');
+
+  const mainClass = needGrayBg ? 'flex-grow-1 bg-light-gray' : 'flex-grow-1';
+
   return (
     <div className="d-flex flex-column" style={{ minHeight: "100vh" }}>
       <NavbarComponent />
-      <main className="flex-grow-1">
+      <main className={mainClass}>
         <Outlet />
       </main>
       <Footer />
